81d2a4aa01cd7ba9d9861f59e2804572e2f83efe
ruslana-semkiv18 <semkivruslana18@gmail.com>
2026-05-16 15:50:44 +0300

    fixed some mistakes

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,13 @@
-export const errorHandler = (err, req, res, next) => {
-  const status = err.status || 500;
+import { HttpError } from 'http-errors';
 
-  res.status(status).json({
-    message: err.message,
-    status: err.status,
-    error: 'Internal Server Error',
+export const errorHandler = (err, req, res, next) => {
+  if (err instanceof HttpError) {
+    return res.status(err.status).json({
+      message: err.message,
+    });
+  }
+
+  res.status(500).json({
+    message: 'Internal Server Error',
   });
 };
